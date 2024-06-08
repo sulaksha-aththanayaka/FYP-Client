023ffe57a4c94593f143f4882cf597dd4ca37286
sulaksha-aththanayaka <sulakshaa04@gmail.com>
2024-06-08 20:04:02 +0530

    Python script added - video play when SearchBar component mounted

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { FaSearch } from "react-icons/fa"
 import axios from 'axios'
+import { spawn } from 'child_process';
 
 export const SearchBar = ({setUrl}) => {
     const [input, setInput] = useState("");
 
     const [results, setResults] = useState([]);
 
+  
+
     const handleSubmit = async () => {
+      // const childPython = spawn('python', ['detect.py']);
+
+      // childPython.stdout.on('data', (data) => {
+      //     console.log("stdout:", data);
+      // });
+
+      const response = await axios.post('http://localhost:5000/api/run-python');
+      const { gender, age } = response.data;
+  
+      console.log(gender);
+      
+
+      console.log(gender);
+      console.log(age);
         try {
           const response = await axios.get('http://localhost:5000/api/fetch-videos', {
             params: {
-              gender: input
+              gender: gender.toLowerCase()
             }
           });
           const results = response.data;
@@ -27,18 +44,22 @@ export const SearchBar = ({setUrl}) => {
         }
       };
 
+      useEffect(() => {
+        handleSubmit();
+      }, []);
+
    
 
   return (
     <div className="pt-2 relative mx-auto text-gray-600">
-        <input value={input} onChange={(e) => setInput(e.target.value)} className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+        {/* <input value={input} onChange={(e) => setInput(e.target.value)} className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
         type="search" name="search" placeholder="Search" />
         <div>
             <button onClick={handleSubmit} type="submit" className="absolute right-0 top-0 mt-5 mr-4">
                 <FaSearch />
             </button>
-        </div>
-        
+        </div> */}
+        <h1>HHHHH</h1>
   </div>
   )
 }
